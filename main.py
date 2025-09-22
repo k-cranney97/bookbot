a5bf6d89ba107bdbc2527a3d5392da0fe3020062
Kyle <k.cranney97@gmail.com>
2025-09-21 21:10:51 -0400

    count letters

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from stats import num_of_words
+from stats import count_letters
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = num_of_words(text)
+    num_letters = count_letters(text)
     print(f"{num_words} words found in the document")
+    print(num_letters)
+
 
 def get_book_text(path):
-    with open(path) as f:
+    with open(path, "r", encoding="utf-8") as f:
         return f.read()
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,12 @@ def num_of_words(text):
     words = text.split()
     return len(words)
 
+def count_letters(text):
+    character = {}
+    for ch in text.lower():
+        if ch in character:
+            character[ch] += 1
+        else:
+            character[ch] = 1
+    return character
+
